0900dc4580cb51419913c5dc36a9145e272cacd2
Bogdan Seniuc <bogdan@bog.dev>
2023-02-05 18:49:46 +0200

    Add plex_debrid addon

---
+++ plex_debrid/CHANGELOG.md
@@ -0,0 +1,6 @@
+<!-- https://developers.home-assistant.io/docs/add-ons/presentation#keeping-a-changelog -->
+
+## 2.70
+
+- Initial release
+- plex_debrid updated to 2.70
---
+++ plex_debrid/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /app/
+s6-svc -wU /var/run/s6/legacy-services/app
+while true; do
+    clear
+    echo "Choose an option:"
+    case "$(gum choose "config - Pause plex_debrid and enter configuration screen" "shell - Enter a shell for addon debugging")" in
+        config*)
+            s6-svc -d /var/run/s6/legacy-services/app
+            mkdir -p /config/addons_config/plex_debrid
+            python3 ./main.py --config-dir /config/addons_config/plex_debrid
+            s6-svc -u /var/run/s6/legacy-services/app
+            ;;
+        shell*)
+            echo "To get back to the initial screen, exit the shell."
+            bash
+            ;;
+    esac
+done
